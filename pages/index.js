2b5b64311acb41d6d1c21b637ac20ad58b645a88
Nausheen Akhter <nausheen701@gmail.com>
2022-08-10 17:39:47 -0400

    button

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <Page>
       <h1>This is the home page</h1>
-      
+      <div>
+        <button onClick={() => addToCart()}>Add to cart</button>
+      </div>
     </Page>
   )
 }
