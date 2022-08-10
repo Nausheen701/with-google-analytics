242b405519bb413fa505060dc7ff47ca791967b5
Nausheen Akhter <nausheen701@gmail.com>
2022-08-10 17:45:29 -0400

    addtocart function

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Page from '../components/Page'
+import * as gtag from '../lib/gtag'
 
 export default function Home() {
+const addToCart = () => {
+  gtag.event({
+    action: "add_to_cart", 
+    category: "ecommerce", 
+    label: "Item added", 
+    value: "Lessons"
+
+  })
+}
+
   return (
     <Page>
       <h1>This is the home page</h1>
